93974f773db8eab71e7b917a4cc03b09bb9adb78
Parker Austin <parkera060@gmail.com>
2023-08-14 11:58:18 -0400

    initalize project

---
+++ script.js
@@ -0,0 +1,6 @@
+// scoring "You" vs "Computer"
+// once user makes selection computer uses random number to pick from array of answers
+// use the user answer and compare to computers random selection
+// user either wins loses or draws
+// must end game once either user or computer gets 4 
+
